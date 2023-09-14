50c46176e10828dfcada5487e6c56e43a21794a9
Gustavo Ferreira <gfmb94@gmail.com>
2023-09-14 11:51:28 -0300

    Aula 21 - Atividade de metodos e delegação pt.2

---
+++ Aulas/Aula21/src/entities/Product.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Product {
+	public String name;
+	public Double price;
+	public Integer quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return name
+			+ ", $ "
+			+ String.format("%.2f", price)
+			+ ", "
+			+ quantity
+			+ " units, Total: $ "
+			+ totalValueInStock();
+	}
+}
