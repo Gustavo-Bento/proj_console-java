478fe456888612b3c8efa1ea03ca52e37295bd5b
Gustavo Ferreira <gfmb94@gmail.com>
2023-09-14 11:46:43 -0300

    Aula 20 - Atividade de metodos e delegação pt.1

--- Aulas/Aula20/src/entities/Product.java
+++ Aulas/Aula20/src/entities/Product.java
@@ -16,4 +16,14 @@ public class Product {
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
+	
+	public String toString() {
+		return name
+			+ ", $ "
+			+ String.format("%.2f", price)
+			+ ", "
+			+ quantity
+			+ "units, Total: $ "
+			+ totalValueInStock();
+	}
 }
